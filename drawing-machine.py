180cc9003a58dd9317f2b5b07b940b32c0b62179
Carlos Cabo <carloscabo@gmail.com>
2016-05-30 22:23:18 +0200

    Drawing machine almost done

--- drawing-machine.py
+++ drawing-machine.py
@@ -3,6 +3,7 @@ import os
 import time
 import math
 import random
+import colorsys
 
 if os.name == 'nt': # Is windows
     import cairo
@@ -20,8 +21,11 @@ class Axis:
         self.r = r
         self.angle_current = random.random() * 2 * math.pi
         self.angle_inc = angle_inc
-    def rotate( self ):
-        self.angle_current += self.angle_inc
+    def rotate( self, angle = 0 ):
+        # if angle == None:
+        #     angle = self.angle_inc
+        self.angle_current += angle
+        return self
     def getPoint( self ):
         x = self.cx + self.r * math.cos( self.angle_current )
         y = self.cy + self.r * math.sin( self.angle_current )
@@ -32,7 +36,6 @@ h = 600
 cx = w / 2
 cy = h / 2
 t = 0
-iteraciones = 1000
 
 ims = cairo.ImageSurface(cairo.FORMAT_ARGB32, w, h)
 ctx = cairo.Context(ims)
@@ -50,9 +53,9 @@ paper_h = h * 0.8
 paper_r = w * 0.8 / 2 # radius
 paper_inc_angle = random.random() * 5 - 2.5 # -2.5 / 2.5
 
-ctx.set_source_rgba(0.9, 0.9, 0.9, 0.5)
+ctx.set_source_rgba(0.9, 0.9, 0.9, 0.2)
 draw.circle( ctx, cx, cy, paper_r)
-ctx.set_source_rgba(0.9, 0, 0, 1.0)
+ctx.set_source_rgba(0.9, 0, 0, 0.2)
 draw.plot( ctx, cx, cy, 2)
 
 # Axis A
@@ -60,59 +63,117 @@ axis_a = Axis(
     cx - paper_r, # - ( ( random.random() *  axis_a_r * 2 ) - axis_a_r )
     (h - paper_h) / 2 + random.random() * paper_h,
     random.random() * ( paper_r / 2 ) + paper_r / 10,
-    random.random() * 5 - 2.5
+    0
 )
 
-ctx.set_source_rgba(0.9, 0.9, 0.9, 0.5)
+ctx.set_source_rgba(0.9, 0.9, 0.9, 0.2)
 draw.circle( ctx, axis_a.cx, axis_a.cy, axis_a.r)
-ctx.set_source_rgba(0.9, 0, 0, 1.0)
+ctx.set_source_rgba(0.9, 0, 0, 0.2)
 draw.plot( ctx, axis_a.cx, axis_a.cy, 2)
 
 axis_b = Axis(
     cx + paper_r, # - ( ( random.random() *  axis_a_r * 2 ) - axis_a_r )
     (h - paper_h) / 2 + random.random() * paper_h,
     random.random() * ( paper_r / 2 ) + paper_r / 10,
-    random.random() * 5 - 2.5
-)
-
-ctx.set_source_rgba(0.9, 0.9, 0.9, 0.5)
-draw.circle( ctx, axis_b.cx, axis_b.cy, axis_b.r)
-ctx.set_source_rgba(0.9, 0, 0, 1.0)
-draw.plot( ctx, axis_b.cx, axis_b.cy, 2)
-
-# H line
-pa = axis_a.getPoint()
-ctx.set_source_rgba(0.9, 0, 0, 1.0)
-draw.plot( ctx, pa[0], pa[1], 2)
-pb = axis_b.getPoint()
-ctx.set_source_rgba(0.9, 0, 0, 1.0)
-draw.plot( ctx, pb[0], pb[1], 2)
-ctx.set_source_rgba(0.9, 0.9, 0.9, 0.5)
-draw.line( ctx, pa[0], pa[1], pb[0], pb[1] )
-axis_distance = utilz.distance( pa[0], pa[1], pb[0], pb[1] )
-
-# center axis
-h_line_cp = utilz.lineLerp( pa[0], pa[1], pb[0], pb[1], 0.5 )
-ctx.set_source_rgba(0.9, 0, 0, 1.0)
-draw.plot( ctx, h_line_cp['x'], h_line_cp['y'], 2)
-
-axis_final = Axis(
-    h_line_cp['x'],
-    h_line_cp['y'],
-    random.random() * axis_distance / 2,
     0
 )
 
-# final point "pen"
-ctx.set_source_rgba(0.9, 0.9, 0.9, 0.5)
+ctx.set_source_rgba(0.9, 0.9, 0.9, 0.2)
+draw.circle( ctx, axis_b.cx, axis_b.cy, axis_b.r)
+ctx.set_source_rgba(0.9, 0, 0, 0.2)
+draw.plot( ctx, axis_b.cx, axis_b.cy, 2)
+
+# Rod (eje que une las ruedas de una locomotora)
+pa = axis_a.getPoint()
+ctx.set_source_rgba(0.9, 0, 0, 0.2)
+draw.plot( ctx, pa[0], pa[1], 2)
+pb = axis_b.getPoint()
+ctx.set_source_rgba(0.9, 0, 0, 0.2)
+draw.plot( ctx, pb[0], pb[1], 2)
+ctx.set_source_rgba(0.9, 0.9, 0.9, 0.2)
+draw.line( ctx, pa[0], pa[1], pb[0], pb[1] )
+rod_length = utilz.distance( pa[0], pa[1], pb[0], pb[1] )
+
+# center axis
+rod_center = utilz.lineLerp( pa[0], pa[1], pb[0], pb[1], 0.5 )
+ctx.set_source_rgba(0.9, 0, 0, 0.2)
+draw.plot( ctx, rod_center['x'], rod_center['y'], 2)
+
+axis_final = Axis(
+    rod_center['x'],
+    rod_center['y'],
+    max(random.random() * paper_w / 2, 60), # (random.random() * rod_length / 4) + rod_length / 6,
+    0
+)
+# axis_final.cx = rod_center['x']
+# axis_final.cy = rod_center['y']
+
+# final point "pencil"
+ctx.set_source_rgba(0.9, 0.9, 0.9, 0.2)
 draw.circle( ctx, axis_final.cx, axis_final.cy, axis_final.r)
-pen = axis_final.getPoint()
-ctx.set_source_rgba(0.9, 0, 0, 1.0)
-draw.plot( ctx, pen[0], pen[1], 2)
-ctx.set_source_rgba(0.9, 0.9, 0.9, 0.5)
-draw.line( ctx,axis_final.cx, axis_final.cy, pen[0], pen[1] )
+pencil = axis_final.getPoint()
+ctx.set_source_rgba(0.9, 0, 0, 0.2)
+draw.plot( ctx, pencil[0], pencil[1], 2)
+ctx.set_source_rgba(0.9, 0.9, 0.9, 0.2)
+draw.line( ctx, axis_final.cx, axis_final.cy, pencil[0], pencil[1] )
+last_x = pencil[0]
+last_y = pencil[1]
 
-# for i in range(iteraciones):
+ctx.set_source_rgba(0.9, 0.9, 0.9, 0.25)
+ctx.set_line_width(1.2)
 
+iteraciones = random.randint(2000, 40000)
+ROT = ( 90 * 2 * math.pi ) / iteraciones
+print(iteraciones)
+
+axis_a_rot_factor = random.randint(1, 12) + 0.005
+axis_b_rot_factor = random.randint(1, 12) + 0.005
+axis_p_rot_factor = random.randint(1, 12) + 0.005
+while axis_a_rot_factor == axis_p_rot_factor or axis_b_rot_factor == axis_p_rot_factor:
+     axis_p_rot_factor = random.randint(2, 12) + 0.005
+
+#print('ROT:' + str(ROT) )
+print('a: ' + str(axis_a_rot_factor))
+print('b: ' + str(axis_b_rot_factor))
+print('p: ' + str(axis_p_rot_factor))
+
+hsl = [ 0.069, 1.0, 0.5 ]
+
+ctx.set_operator(cairo.OPERATOR_SCREEN)
+h = random.random()
+
+for i in range(iteraciones):
+
+    rotation = i * ROT
+
+    pa = axis_a.rotate( ROT * axis_a_rot_factor ).getPoint()
+    pb = axis_b.rotate( ROT * axis_b_rot_factor ).getPoint()
+    rod_center = utilz.lineLerp( pa[0], pa[1], pb[0], pb[1], 0.5 )
+    axis_final.cx = rod_center['x']
+    axis_final.cy = rod_center['y']
+    pencil = axis_final.rotate( ROT * axis_p_rot_factor  ).getPoint()
+
+    pencil_r = utilz.rotatePoint( cx, cy,  pencil[0], pencil[1], rotation )
+
+    # ctx.save()
+    # ctx.translate( w/2, h/2)
+    # ctx.rotate( i * 0.0174533)
+
+    s = 1 - (utilz.distance( cx, cy,  pencil[0], pencil[1] ) / (paper_w / 2))
+    rgb = colorsys.hsv_to_rgb( h, s, 0.5)
+    ctx.set_source_rgba( *rgb, 0.25)
+
+    ctx.move_to( last_x, last_y )
+    ctx.line_to( pencil_r[0], pencil_r[1] )
+    ctx.stroke()
+    last_x = pencil_r[0]
+    last_y = pencil_r[1]
+    # draw.plot( ctx, pencil[0], pencil[1], 1)
+
+    # ctx.restore()
+
+# pencil = axis_final.getPoint()
+# ctx.set_source_rgba(0, 0.9, 0, 1.0)
+# draw.plot( ctx, pencil[0], pencil[1], 2)
 
 ims.write_to_png("test.png")
