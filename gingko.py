740c88ead4613693a1025393c860b9dfd4c046e5
Carlos Cabo <carloscabo@gmail.com>
2016-06-10 19:15:34 +0200

    Adding Gingko algorithm

--- gingko.py
+++ gingko.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 import os
 import time
 import math
@@ -11,13 +11,17 @@ else:
     import cairocffi as cairo
 
 from mod import draw
+from mod.colz import *
 
 w = 600
 h = 600
 cx = w / 2
 cy = h / 2
 
-now = time.time()
+col = Colz()
+col.setHsla( random.randint(0, 359), 51, 50, 0.25)
+
+num_of_arms = random.randint(2, 9)
 
 ims = cairo.ImageSurface(cairo.FORMAT_ARGB32, w, h) # Simple Surface
 ctx = cairo.Context(ims)
@@ -27,50 +31,59 @@ ctx.rectangle (0, 0, w, h) # Rectangle(x0, y0, x1, y1)
 ctx.set_source_rgb(0.1, 0.1, 0.1)
 ctx.fill ()
 
-ctx.set_operator(cairo.OPERATOR_ADD)
+ctx.set_operator(cairo.OPERATOR_OVER)
+ctx.set_operator(cairo.OPERATOR_SCREEN)
+ctx.set_line_width(1.0)
 
 class Arm:
-    def __init__ (self, cx, cy, angle = math.pi/2, segments = 250, dis = 2.0):
+    def __init__ (self, cx, cy):
         self.cx = cx
         self.cy = cy
+        self.segments = random.randint( 1000, 5000)
+        self.dis = random.uniform(0.25, 0.65)
+        self.segment_w = random.randint(50, 250)
+        self.segment_w_current = self.segment_w
+        self.noise_seed = random.random() * 10.0
+        self.noise_speed = 4.0 + ( random.random() * 6.0 )
+        self.noise_base = random.randint(1, 16)
+
+        if num_of_arms < 4:
+            self.segment_w *= 3
+
+    def render(self, ctx):
         self.current_x = cx
         self.current_y = cy
         self.prev_x = cx
         self.prev_y = cy
-        self.angle = random.random() * math.pi*2
-        self.segments = segments
-        self.segment_w = 100.0
-        self.segment_w_current = self.segment_w
-        self.dis = dis
-
-    def render(self, ctx):
-        ctx.set_source_rgba(0.9, 0.9, 0.9, 0.5)
-        seed = random.random() * 9.99
+        self.angle = 0 # random.random() * math.pi*2
 
         for i in range(self.segments):
 
             pn1 = pnoise1 (
-                seed + ( i / self.segments * 8 ),
+                self.noise_seed + ( (i / self.segments) * self.noise_speed ),
                 2,
                 0.2,  # persintence 0.2
                 4.0,  # Lacunarity 2.0
-                256,   # repeat 1024
-                # 2
+                1024,   # repeat 1024
+                self.noise_base
             )
-            print(str(pn1))
+            # print(str(pn1))
 
             _cos = math.cos(self.angle)
             _sin = math.sin(self.angle)
             self.current_x = self.current_x + self.dis * _cos
             self.current_y = self.current_y + self.dis * _sin
-            # print(str(self.current_x) + ' ' + str(self.current_y))
 
-            # self.angle += random.random() * 0.1 - 0.05
-            self.angle += pn1 / 10
-            ctx.set_source_rgba(0.9, 0.9, 0.9, 0.25)
+            self.angle += pn1 / 60.0
+
+            self.segment_w_current = self.segment_w * pn1 * 2
+
+            col.a = 0.5 + (pn1 * 0.5)
+            col.rotateHue( pn1 / random.randint( 200, 4000) )
+            # col.setSat( 0.0 )
+            ctx.set_source_rgba( *col.rgba)
             # draw.plot( ctx, self.current_x, self.current_y, 1)
 
-            # if i != 0:
             ABx =  self.current_x - self.prev_x
             ABy =  self.current_y - self.prev_y
             NABx = ABx / self.dis
@@ -83,16 +96,21 @@ class Arm:
             Dy2 = self.current_y - self.segment_w_current * PNABy
             draw.line( ctx, Dx1, Dy1, Dx2, Dy2)
 
-            #self.segment_w_current = self.segment_w * ( 1.0 - i / self.segments )
-
             self.prev_x = self.current_x
             self.prev_y = self.current_y
             # draw.line( ctx, self.cx, self.cy, self.cx + 30, self.cy )
 
-arm1 = Arm( cx, cy, )
-arm1.render( ctx )
-
-ctx.set_source_rgba(0.9, 0.9, 0.9, 0.5)
-draw.plot( ctx, cx, cy, 1)
+arm1 = Arm( 0, 0 )
+ctx.save()
+for i in range(num_of_arms):
+    ctx.save()
+    ctx.translate(cx, cy)
+    ctx.rotate( 2 * math.pi / num_of_arms * i )
+    ctx.translate(-cx, -cy)
+    arm1.render( ctx )
+    ctx.restore()
+print(num_of_arms)
+# ctx.set_source_rgba(0.9, 0.9, 0.9, 0.5)
+# draw.plot( ctx, cx, cy, 1)
 
 ims.write_to_png("test.png")
