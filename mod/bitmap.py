8fe20d9e0b3a33e6c0f346698759d894bd3c1867
Carlos Cabo <carloscabo@gmail.com>
2016-06-11 11:49:05 +0200

    Before triying to use cairocffi in windows too

--- mod/bitmap.py
+++ mod/bitmap.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import math
+import struct
 
 if os.name == 'nt': # Is windows
     import cairo
@@ -17,11 +19,23 @@ def negative(surface):
         buf[i+1] = 255 - buf[i+1]
         buf[i+2] = 255 - buf[i+2]
 
+def bytes2int(str):
+    return int(str.encode('hex'), 16)
+
 # Loops over all the pixels measuring a float value combining rgb
-# 
 def getLuminanceMedia(surface):
     buf = surface.get_data()
     pixel = 0.0
     for i in range(0,len(buf),4):
-        pixel += ( ( buf[i]/255 + buf[i+1]/255 + buf[i+2]/255 ) / 3 )
+        # In some environments pixel data ir returned as by in others as int :(
+        r = buf[i]
+        if isinstance( r, bytes ):
+            r = int.from_bytes( r, byteorder='big' )
+        g = buf[i+1]
+        if isinstance( g, bytes ):
+            g = int.from_bytes( g, byteorder='big' )
+        b = buf[i+2]
+        if isinstance( b, bytes ):
+            b = int.from_bytes( b, byteorder='big' )
+        pixel += ( ( r / 255.0 + g / 255.0 + b / 255.0 ) / 3.0 )
     return pixel / (len(buf)/4)
