aa2c1e2ac186ec6279df1ddcd2213783279c6e2c
Carlos Cabo <carloscabo@gmail.com>
2016-06-25 11:26:24 +0200

    Tested all algorithms with cairocffi in windows

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ import importlib
 drawing_modules = {
     # name, probability
     # 'algorithm_chrysanthemum': 0, # Disabled
-    # 'algorithm_param_eq': 1,
+    'algorithm_param_eq': 1,
     'algorithm_perlin_brush': 1,
-    # 'algorithm_drawing_machine': 2,
-    # 'algorithm_gingko': 2,
-    # 'algorithm_zircles': 3,
+    'algorithm_drawing_machine': 2,
+    'algorithm_gingko': 2,
+    'algorithm_zircles': 3,
 }
 temp = []
 for name, prob in drawing_modules.items():
